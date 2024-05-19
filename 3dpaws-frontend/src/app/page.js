ae5db02983093adf56974ebc815c51466a989701
Saurav <alexthundrous2104@gmail.com>
2024-05-19 17:09:56 +0530

    Added graph - Burger menu is bugged

--- 3dpaws-frontend/src/app/page.js
+++ 3dpaws-frontend/src/app/page.js
@@ -1,7 +1,7 @@
-import Navbar from '../components/navbar';
-import Header from '../components/header';
-import Stations from '../components/stations';
-
+import Navbar from '@/components/navbar';
+import Header from '@/components/header';
+import Stations from '@/components/stations';
+import ScatterPlot from '@/components/graph';
 
 const stationData = [
   {"Name": "WxPi01", "Number": 1, "isGreen": "True"},
@@ -86,33 +86,36 @@ export default function Home() {
             <Stations key={index} Name={station.Name} Number={station.Number} isGreen={station.isGreen} />
           ))}
       </div>
-         <div className="flex justify-center mx-auto overflow-y-hidden mt-8">
-          <div className="bg-orange-400 w-40 h-[3%] rounded-lg flex flex-col mr-2" >
+        <div className='flex flex-col items-center w-[76%] h-full'>
+         <div className="flex justify-center mx-auto overflow-y-hidden mt-8 mb-8">
+          <div className="bg-orange-400 w-40 h-40 rounded-lg flex flex-col mr-2" >
             <h1 className="mt-auto mx-auto">Measurements</h1>
             <h2 className="mb-auto mx-auto text-blue-900">772,284,666</h2>
           </div>
-          <div className="bg-orange-400 w-40 h-[3%] rounded-lg flex flex-col mr-2" >
+          <div className="bg-orange-400 w-40 h-40 rounded-lg flex flex-col mr-2" >
             <h1 className="mt-auto mx-auto">Instruments</h1>
             <h2 className="mb-auto mx-auto text-blue-900">103</h2>
           </div>
-          <div className="bg-orange-400 w-40 h-[3%] rounded-lg flex flex-col mr-2" >
+          <div className="bg-orange-400 w-40 h-40 rounded-lg flex flex-col mr-2" >
             <h1 className="mt-auto mx-auto">Sites</h1>
             <h2 className="mb-auto mx-auto text-blue-900">78</h2>
           </div>
-          <div className="bg-orange-400 w-40 h-[3%] rounded-lg flex flex-col mr-2" >
+          <div className="bg-orange-400 w-40 h-40 rounded-lg flex flex-col mr-2" >
             <h1 className="mt-auto mx-auto">Storage</h1>
             <h2 className="mb-auto mx-auto text-blue-900">1281.3 MB</h2>
           </div>
-          <div className="bg-orange-400 w-40 h-[3%] rounded-lg flex flex-col mr-2" >
+          <div className="bg-orange-400 w-40 h-40 rounded-lg flex flex-col mr-2" >
             <h1 className="mt-auto mx-auto">DB Expires </h1>
             <h2 className="mb-auto mx-auto text-blue-900"> 2022-05-20 </h2>
           </div>
-          <div className="bg-orange-400 w-40 h-[3%] rounded-lg flex flex-col mr-2" >
+          <div className="bg-orange-400 w-40 h-35 rounded-lg flex flex-col mr-2" >
             <h1 className="mt-auto mx-auto">Uptime </h1>
             <h2 className="mb-auto mx-auto text-blue-900"> 8 days </h2>
            </div >
+         </div>
+          <ScatterPlot/>
         </div>
-     </div>
+        </div>
     </main>
   );
 }
---
+++ 3dpaws-frontend/src/components/graph.jsx
@@ -0,0 +1,113 @@
+"use client";
+import { useRef } from 'react';
+import { Scatter } from 'react-chartjs-2';
+import classNames from 'classnames';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LinearScale,
+  TimeScale,
+  registerables
+} from 'chart.js';
+import 'chartjs-adapter-date-fns';
+
+ChartJS.register(...registerables);
+
+const ScatterPlot = () => {
+  const chartRef = useRef(null);
+  const opened = false; 
+  const data = {
+    datasets: [
+      {
+        label: 'Samples per minute',
+        data: [
+          { x: new Date('2023-05-19T14:50:00'), y: 400 },
+          { x: new Date('2023-05-19T14:50:00'), y: 350 },
+          { x: new Date('2023-05-19T14:51:00'), y: 300 },
+          { x: new Date('2023-05-19T14:52:00'), y: 250 },
+        ],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+        pointRadius: 10,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'minute',
+          tooltipFormat: 'HH:mm',
+          displayFormats: {
+            minute: 'HH:mm',
+          },
+          min: new Date('2023-05-19T14:40:00'), 
+        },
+        title: {
+          display: true,
+          text: 'Time',
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: 'Samples per minute',
+        },
+        beginAtZero: true,
+        ticks: {
+          stepSize: 100, 
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            return `${context.raw.x}: ${context.raw.y} samples`;
+          },
+        },
+      },
+    },
+  };
+
+  const downloadChart = (format) => {
+    if (chartRef.current) {
+      const chartInstance = chartRef.current;
+      let url;
+      if (format === 'jpg') {
+        url = chartInstance.toBase64Image('image/jpeg');
+      } else {
+        url = chartInstance.toBase64Image();
+      }
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = `chart.${format}`;
+      link.click();
+    }
+  };
+
+  return (
+    <div className="relative w-full h-70vh">
+      <div className={classNames(` absolute top-0 right-0 tham tham-e-squeeze tham-w-6`, { 'tham-active': opened })}>
+        <div className="tham-box">
+        <div className="tham-inner flex flex-col">  
+            <button onClick={() => downloadChart('png')} className="bg-gray-800 text-white ">PNG</button>
+            <button onClick={() => downloadChart('jpg')} className="bg-gray-800 text-white">JPG</button>
+        </div>  
+        </div>
+      </div>
+      <Scatter ref={chartRef} data={data} options={options} />
+    </div>
+  );
+};
+
+export default ScatterPlot;
